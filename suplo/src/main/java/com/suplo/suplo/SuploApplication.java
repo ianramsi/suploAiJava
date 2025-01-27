14f3f83ce780043367e831acca7abe2aa5ae4a91
Suplo Silpo <ian.ramsianto@gmail.com>
2025-01-27 08:42:39 +0000

    Initial Commit

---
+++ suplo/src/main/java/com/suplo/suplo/SuploApplication.java
@@ -0,0 +1,13 @@
+package com.suplo.suplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuploApplication.class, args);
+	}
+
+}
---
+++ suplo/src/test/java/com/suplo/suplo/SuploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suplo.suplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
